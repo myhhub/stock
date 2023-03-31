cd4d6710e530f98a15e6beb75bdd903d84da36ad
myhhub <yh_ma@qq.com>
2023-03-31 23:14:59 +0800

    指标优化

--- web/dataIndicatorsHandler.py
+++ web/dataIndicatorsHandler.py
@@ -5,15 +5,14 @@ from abc import ABC
 import numpy as np
 from tornado import gen
 import logging
-import datetime
 import pandas as pd
 # 首映 bokeh 画图。
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.palettes import Spectral11
-from bokeh.layouts import gridplot, column, row
+from bokeh.layouts import column, row
 from bokeh.models import DatetimeTickFormatter, ColumnDataSource, HoverTool, CheckboxGroup, LabelSet, Button, CustomJS, \
-    CDSView, BooleanFilter
+    CDSView, BooleanFilter, TabPanel, Tabs
 import libs.stockfetch as stf
 import libs.common as common
 import libs.tablestructure as tbs
@@ -49,127 +48,127 @@ stats_dic = [
 # 全部指标数据汇总
 indicators_dic = [
     {
-        "title": "1、MACD指标",
+        "title": "MACD",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/MACD" rel="nofollow" target="_blank">平滑异同移动平均线(Moving Average Convergence Divergence，简称MACD指标)</a>
     """,
-        "dic": [("close",), ("macd", "macds", "macdh")]
+        "dic": ("macd", "macds", "macdh")
     }, {
-        "title": "2、KDJ指标",
+        "title": "KDJ",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/%E9%9A%8F%E6%9C%BA%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">随机指标(KDJ)</a>
     """,
-        "dic": [("close",), ("kdjk", "kdjd", "kdjj")]
+        "dic": ("kdjk", "kdjd", "kdjj")
     }, {
-        "title": "3、BOLL指标",
+        "title": "BOLL",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/BOLL" rel="nofollow" target="_blank">布林线指标(Bollinger Bands)</a>
     """,
-        "dic": [("close", "boll_ub", "boll", "boll_lb")]
+        "dic": ("close", "boll_ub", "boll", "boll_lb")
     }, {
-        "title": "4、TRIX，TRMA指标",
+        "title": "TRIX",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/TRIX" rel="nofollow" target="_blank">TRIX指标又叫三重指数平滑移动平均指标（Triple Exponentially Smoothed Average）</a>
     """,
-        "dic": [("close",), ("trix", "trix_20_sma")]
+        "dic": ("trix", "trix_20_sma")
     }, {
-        "title": "5、CR指标",
+        "title": "CR",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/CR%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">价格动量指标(CR)</a>
     """,
-        "dic": [("close",), ("cr", "cr-ma1", "cr-ma2", "cr-ma3")]
+        "dic": ("cr", "cr-ma1", "cr-ma2", "cr-ma3")
     }, {
-        "title": "6、SMA指标",
+        "title": "SMA",
         "desc": """
             <a href="http://wiki.mbalib.com/wiki/Sma" rel="nofollow" target="_blank">简单移动平均线（Simple Moving Average，SMA）</a> 
         """,
-        "dic": [("close", "close_10_sma", "close_20_sma", "close_50_sma", "close_200_sma")]
+        "dic": ("close", "close_10_sma", "close_20_sma", "close_50_sma", "close_200_sma")
     }, {
-        "title": "7、RSI指标",
+        "title": "RSI",
         "desc": """
             <a href="http://wiki.mbalib.com/wiki/RSI" rel="nofollow" target="_blank">相对强弱指标（Relative Strength Index，简称RSI）</a> 
         """,
-        "dic": [("close",), ("rsi_6", "rsi_12", "rsi", "rsi_24")]
+        "dic": ("rsi_6", "rsi_12", "rsi", "rsi_24")
     }, {
-        "title": "8、VR，MAVR指标",
+        "title": "VR",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/%E6%88%90%E4%BA%A4%E9%87%8F%E6%AF%94%E7%8E%87" rel="nofollow" target="_blank">成交量比率（Volumn Ratio，VR）（简称VR）</a>
     """,
-        "dic": [("close",), ("vr", "vr_6_sma")]
+        "dic": ("vr", "vr_6_sma")
     }, {
-        "title": "9、ROC指标",
+        "title": "ROC",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/ROC" target="_blank">ROC指标</a>
         """,
-        "dic": [("close",), ("roc", "rocma", "rocema")]
+        "dic": ("roc", "rocma", "rocema")
     }, {
-        "title": "10、DMI，+DI，-DI，DX，ADX，ADXR指标",
+        "title": "DMI",
         "desc": """
         <a href="http://wiki.mbalib.com/wiki/DMI" rel="nofollow" target="_blank">动向指数Directional Movement Index,DMI）</a>
         <a href="http://wiki.mbalib.com/wiki/ADX" rel="nofollow" target="_blank">平均趋向指标（Average Directional Indicator，简称ADX）</a>
         <a href="http://wiki.mbalib.com/wiki/%E5%B9%B3%E5%9D%87%E6%96%B9%E5%90%91%E6%8C%87%E6%95%B0%E8%AF%84%E4%BC%B0" rel="nofollow" target="_blank">平均方向指数评估（ADXR）</a>
     """,
-        "dic": [("close",), ("pdi", "mdi", "dx", "adx", "adxr")]
+        "dic": ("pdi", "mdi", "dx", "adx", "adxr")
     }, {
-        "title": "11、W&R指标",
+        "title": "W&R",
         "desc": """
             <a href="http://wiki.mbalib.com/wiki/%E5%A8%81%E5%BB%89%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">威廉指数（Williams%Rate）</a>
         """,
-        "dic": [("close",), ("wr_6", "wr_10", "wr_14")]
+        "dic": ("wr_6", "wr_10", "wr_14")
     }, {
-        "title": "12、CCI指标",
+        "title": "CCI",
         "desc": """
             <a href="http://wiki.mbalib.com/wiki/%E9%A1%BA%E5%8A%BF%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">顺势指标(CCI)</a>
         """,
-        "dic": [("close",), ("cci", "cci_84")]
+        "dic": ("cci", "cci_84")
     }, {
-        "title": "13、TR、ATR指标",
+        "title": "ATR",
         "desc": """
            <a href="http://wiki.mbalib.com/wiki/%E5%9D%87%E5%B9%85%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">均幅指标（Average True Ranger,ATR）</a>
         """,
-        "dic": [("close",), ("tr", "atr")]
+        "dic": ("tr", "atr")
     }, {
-        "title": "14、DMA、AMA指标",
+        "title": "DMA",
         "desc": """
             <a href="http://wiki.mbalib.com/wiki/DMA" rel="nofollow" target="_blank">DMA指标（Different of Moving Average）又叫平行线差指标</a> 
         """,
-        "dic": [("close",), ("dma", "dma_10_sma")]
+        "dic": ("dma", "dma_10_sma")
     }, {
-        "title": "15、OBV指标",
+        "title": "OBV",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/OBV" rel="nofollow" target="_blank">OBV指标</a>
         """,
-        "dic": [("close",), ("obv",)]
+        "dic": ("obv",)
     }, {
-        "title": "16、SAR指标",
+        "title": "SAR",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/SAR%E6%8C%87%E6%A0%87" rel="nofollow" target="_blank">SAR指标</a>
         """,
-        "dic": [("close", "sar")]
+        "dic": ("close", "sar")
     }, {
-        "title": "17、PSY指标",
+        "title": "PSY",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/PSY" rel="nofollow" target="_blank">PSY指标</a>
         """,
-        "dic": [("close",), ("psy",)]
+        "dic": ("psy",)
     }, {
-        "title": "18、BRAR指标",
+        "title": "BRAR",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/BRAR%E6%8C%87%E6%A0%87" target="_blank">BRAR指标</a>
         """,
-        "dic": [("close",), ("br", "ar")]
+        "dic": ("br", "ar")
     }, {
-        "title": "19、EMV指标",
+        "title": "EMV",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/%E7%AE%80%E6%98%93%E6%B3%A2%E5%8A%A8%E6%8C%87%E6%A0%87" target="_blank">EMV指标</a>
         """,
-        "dic": [("close",), ("emv", "emva")]
+        "dic": ("emv", "emva")
     }, {
-        "title": "20、BIAS指标",
+        "title": "BIAS",
         "desc": """
             <a href="https://wiki.mbalib.com/wiki/BIAS%E6%8C%87%E6%A0%87" target="_blank">BIAS指标</a>
         """,
-        "dic": [("close",), ("bias", "bias_12", "bias_24")]
+        "dic": ("bias", "bias_12", "bias_24")
     }
 ]
 
@@ -195,7 +194,7 @@ class GetDataIndicatorsHandler(webBase.BaseHandler, ABC):
 
             r_i = add_indicators(stock, date, threshold)
             if r_i is not None:
-                comp_list.extend(r_i)
+                comp_list.append(r_i)
         except Exception as e:
             logging.debug("{}处理异常：{}".format('dataIndicatorsHandler.GetDataIndicatorsHandler', e))
 
@@ -210,43 +209,37 @@ def add_indicators(stock, date, threshold):
     if data is None:
         return None
 
-    data['date'] = pd.to_datetime(data.index.values)
-    data_list = []
+    data['date'] = data.index.values
+    data['index'] = list(np.arange(len(data)))
+
+    length = len(data)
+    tabs = []
     for conf in indicators_dic:
-        data_list.append(add_plot(data, conf))
-    # for conf in stats_dic:
-    #     data_list.append(add_plot(data, conf))
-    return data_list
-
-
-def add_plot(data, conf):
-    p_list = []
-    # 循环 多个line 信息。
-    for val in conf["dic"]:
-        p = figure(width=1000, height=180, x_axis_type="datetime", toolbar_location='right')
-        for name, color in zip(val, Spectral11):
+        p = figure(width=1000, height=200, x_range=(0, length + 1), toolbar_location=None)
+        for name, color in zip(conf["dic"], Spectral11):
             if name == 'macdh':
                 up = [True if val > 0 else False for val in data[name]]
                 down = [True if val < 0 else False for val in data[name]]
                 view_upper = CDSView(filter=BooleanFilter(up))
                 view_lower = CDSView(filter=BooleanFilter(down))
-                p.vbar(x='date', top=0, bottom=name, legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA), color='green', source=data, view=view_lower)
-                p.vbar(x='date', top=name, legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA), bottom=0, color='red', source=data, view=view_upper)
+                p.vbar('index', 0.1, 0, name, legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA),
+                       color='green', source=data, view=view_lower)
+                p.vbar('index', 0.1, name, 0, legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA),
+                       color='red', source=data, view=view_upper)
             else:
-                p.line(x=data['date'], y=data[name], legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA), color=color,
-                       line_width=1.5, alpha=0.8)
-        p.xaxis[0].formatter = DatetimeTickFormatter(days="%Y-%m-%d")
+                p.line(x=data['index'], y=data[name], legend_label=tbs.get_field_cn(name, tbs.STOCK_STATS_DATA),
+                       color=color, line_width=1.5, alpha=0.8)
         p.legend.location = "top_left"
         p.legend.click_policy = "hide"
-        p_list.append([p])
+        p.xaxis.visible = False
+        p.min_border_bottom = 0
 
-    gp = gridplot(p_list)
-    script, div = components(gp)
+        tabs.append(TabPanel(child=p, title=conf["title"]))
+
+    script, div = components(Tabs(tabs=tabs, tabs_location='below', width=1050, height=230))
     return {
         "script": script,
-        "div": div,
-        "title": conf["title"],
-        "desc": conf["desc"]
+        "div": div
     }
 
 
@@ -287,7 +280,7 @@ def add_kline(stock, date, threshold):
         dec_source = ColumnDataSource(data.loc[dec])
 
         length = len(data)
-        p = figure(width=1000, height=320, x_range=(0, length + 1), toolbar_location='above')
+        p = figure(width=1000, height=320, x_range=(0, length + 1), toolbar_location=None)
         hover = HoverTool(tooltips=[('日期', '@date'), ('开盘', '@open'),
                                     ('最高', '@high'), ('最低', '@low'),
                                     ('收盘', '@close')])
@@ -345,7 +338,7 @@ def add_kline(stock, date, threshold):
                     isHas = True
             if isHas:
                 i += 1
-        p.xaxis.visible = False  # 隐藏x-axis
+        p.xaxis.visible = False
         p.min_border_bottom = 0
 
         p1 = figure(width=p.width, height=150, x_range=p.x_range, toolbar_location=None)
@@ -374,12 +367,7 @@ def add_kline(stock, date, threshold):
         script, div = components(layouts)
         return {
             "script": script,
-            "div": div,
-            "title": "K线",
-            "desc": """
-                <a href="https://wiki.mbalib.com/wiki/K%E7%BA%BF%E7%90%86%E8%AE%BA" rel="nofollow" target="_blank">K线图</a>
-                ，识别股票K线61种K线形态。
-            """
+            "div": div
         }
     except Exception as e:
         logging.debug("{}处理异常：{}".format('dataIndicatorsHandler.add_kline', e))
