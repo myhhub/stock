90d7327fef902c02f4a5c340398aa8dc600fc775
myhhub <yh_ma@qq.com>
2023-04-01 20:57:24 +0800

    指标计算改采用talib

--- indicator/stockstats_data.py
+++ indicator/stockstats_data.py
@@ -3,7 +3,6 @@
 
 import logging
 import pandas as pd
-import stockstats
 import numpy as np
 import talib as tl
 
@@ -11,175 +10,145 @@ __author__ = 'myh '
 __date__ = '2023/3/10 '
 
 
-def get_indicators(data, end_date=None, threshold=60):
+def get_indicators(data, end_date=None, threshold=120, calc_threshold=None):
+    isCopy = False
     if end_date is not None:
         mask = (data['date'] <= end_date)
         data = data.loc[mask]
-    data = data.tail(n=threshold).copy()
+        isCopy = True
+    if calc_threshold is not None:
+        data = data.tail(n=calc_threshold)
+        isCopy = True
+
+    if isCopy:
+        data = data.copy()
     data.loc[:, "volume"] = data["volume"] * 100  # 成交量单位从手变成股。
-    data_stat = stockstats.StockDataFrame.retype(data)
 
-    if data_stat is None or len(data_stat.index) == 0:
-        return None
+    # import stockstats
+    # test = data.copy()
+    # test = stockstats.StockDataFrame.retype(test) #验证计算结果
 
-    data_stat.loc[:, 'obv'] = tl.OBV(data_stat['close'], data_stat['volume'])
-    data_stat.loc[:, 'sar'] = tl.SAR(data_stat['high'], data_stat['low'])
+    # macd
+    data.loc[:, 'macd'], data.loc[:, 'macds'], data.loc[:, 'macdh'] = tl.MACD(
+        data['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    # kdjk
+    data.loc[:, 'kdjk'], data.loc[:, 'kdjd'] = tl.STOCH(
+        data['high'], data['low'], data['close'], fastk_period=9,
+        slowk_period=5, slowk_matype=1, slowd_period=5, slowd_matype=1)
+    data.loc[:, 'kdjj'] = 3 * data['kdjk'] - 2 * data['kdjd']
+    # boll
+    data.loc[:, 'boll_ub'], data.loc[:, 'boll'], data.loc[:, 'boll_lb'] = tl.BBANDS \
+        (data['close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    # trix
+    data.loc[:, 'trix'] = tl.TRIX(data['close'], timeperiod=12)
+    data.loc[:, 'trix_20_sma'] = tl.MA(data['trix'], timeperiod=20)
+    # cr
+    data.loc[:, 'm_price'] = data['amount'] / data['volume']
+    data.loc[:, 'm_price_sf1'] = data['m_price'].shift(1)
+    data.loc[:, 'h_m'] = data['high'] - data[['m_price_sf1', 'high']].min(axis=1)
+    data.loc[:, 'm_l'] = data['m_price_sf1'] - data[['m_price_sf1', 'low']].min(axis=1)
+    data.loc[:, 'h_m_sum'] = data['h_m'].rolling(window=26).sum()
+    data.loc[:, 'm_l_sum'] = data['m_l'].rolling(window=26).sum()
+    data.loc[:, 'cr'] = (data['h_m_sum'] / data['m_l_sum']) * 100
+    data.loc[:, 'cr-ma1'] = tl.MA(data['cr'], timeperiod=5)
+    data.loc[:, 'cr-ma2'] = tl.MA(data['cr'], timeperiod=10)
+    data.loc[:, 'cr-ma3'] = tl.MA(data['cr'], timeperiod=20)
+    # rsi
+    data.loc[:, 'rsi'] = tl.RSI(data['close'], timeperiod=14)
+    data.loc[:, 'rsi_6'] = tl.RSI(data['close'], timeperiod=6)
+    data.loc[:, 'rsi_12'] = tl.RSI(data['close'], timeperiod=12)
+    data.loc[:, 'rsi_24'] = tl.RSI(data['close'], timeperiod=24)
+    # vr
+    data.loc[:, 'av'] = data['volume'].where(data['p_change'] > 0, 0, inplace=False)
+    data.loc[:, 'avs'] = data['av'].rolling(min_periods=1, window=26, center=False).sum()
+    data.loc[:, 'bv'] = data['volume'].where(data['p_change'] < 0, 0, inplace=False)
+    data.loc[:, 'bvs'] = data['bv'].rolling(min_periods=1, window=26, center=False).sum()
+    data.loc[:, 'cv'] = data['volume'].where(data['p_change'] == 0, 0, inplace=False)
+    data.loc[:, 'cvs'] = data['cv'].rolling(min_periods=1, window=26, center=False).sum()
+    data.loc[:, 'vr'] = ((data['avs'] + data['cvs'] / 2) / (data['bvs'] + data['cvs'] / 2)).replace(np.nan, 0).replace(np.inf, 0) * 100
+    data.loc[:, 'vr_6_sma'] = tl.MA(data['vr'], timeperiod=6)
+    # DMI 计算方法和结果和stockstats不同，stockstats采用每天均价计算
+    data.loc[:, 'pdi'] = tl.PLUS_DI(data['high'], data['low'], data['close'], timeperiod=14)
+    data.loc[:, 'mdi'] = tl.MINUS_DI(data['high'], data['low'], data['close'], timeperiod=14)
+    data.loc[:, 'dx'] = tl.DX(data['high'], data['low'], data['close'], timeperiod=14)
+    data.loc[:, 'adx'] = tl.ADX(data['high'], data['low'], data['close'], timeperiod=6)
+    data.loc[:, 'adxr'] = tl.ADXR(data['high'], data['low'], data['close'], timeperiod=6)
+    # wr
+    data.loc[:, 'h_6'] = data['high'].rolling(window=6).max()
+    data.loc[:, 'l_6'] = data['low'].rolling(window=6).min()
+    data.loc[:, 'wr_6'] = ((data['h_6'] - data['close']) / (data['h_6'] - data['l_6'])) * 100
+    data.loc[:, 'h_10'] = data['high'].rolling(window=10).max()
+    data.loc[:, 'l_10'] = data['low'].rolling(window=10).min()
+    data.loc[:, 'wr_10'] = ((data['h_10'] - data['close']) / (data['h_10'] - data['l_10'])) * 100
+    data.loc[:, 'h_14'] = data['high'].rolling(window=14).max()
+    data.loc[:, 'l_14'] = data['low'].rolling(window=14).min()
+    data.loc[:, 'wr_14'] = ((data['h_14'] - data['close']) / (data['h_14'] - data['l_14'])) * 100
+    # cci
+    data.loc[:, 'cci'] = tl.CCI(data['high'], data['low'], data['close'], timeperiod=14)
+    data.loc[:, 'cci_84'] = tl.CCI(data['high'], data['low'], data['close'], timeperiod=84)
+    # atr
+    data.loc[:, 'prev_close'] = data['close'].shift(1)
+    data.loc[:, 'h_l'] = data['high'] - data['low']
+    data.loc[:, 'h_pc'] = (data['high'] - data['prev_close']).abs()
+    data.loc[:, 'l_pc'] = (data['prev_close'] - data['low']).abs()
+    data.loc[:, 'tr'] = data.loc[:, ['h_l', 'h_pc', 'l_pc']].T.max()
+    data.loc[:, 'atr'] = data['tr'].ewm(ignore_na=False, alpha=1.0 / 14, min_periods=0, adjust=True).mean()
+    # dma
+    data.loc[:, 'ma10'] = tl.MA(data['close'], timeperiod=10)
+    data.loc[:, 'ma50'] = tl.MA(data['close'], timeperiod=50)
+    data.loc[:, 'dma'] = data['ma10'] - data['ma50']
+    data.loc[:, 'dma_10_sma'] = tl.MA(data['dma'], timeperiod=10)
+    # ----------stockstats没有以下指标-----------------
+    # roc
+    data.loc[:, 'roc'] = tl.ROC(data['close'], timeperiod=12)
+    data.loc[:, 'rocma'] = tl.MA(data['roc'], timeperiod=6)
+    data.loc[:, 'rocema'] = tl.EMA(data['rocma'], timeperiod=9)
+    # obv
+    data.loc[:, 'obv'] = tl.OBV(data['close'], data['volume'])
+    # sar
+    data.loc[:, 'sar'] = tl.SAR(data['high'], data['low'])
+    # psy
+    data.loc[:, 'price_up'] = 0
+    data.loc[data['close'] > data['prev_close'], 'price_up'] = 1
+    data.loc[:, 'price_up_sum'] = data['price_up'].rolling(window=12).sum()
+    data.loc[:, 'psy'] = (data['price_up_sum'] / 12.0) * 100
+    # BRAR
+    data.loc[:, 'h_o'] = data['high'] - data['open']
+    data.loc[:, 'o_l'] = data['open'] - data['low']
+    data.loc[:, 'h_o_sum'] = data['h_o'].rolling(window=26).sum()
+    data.loc[:, 'o_l_sum'] = data['o_l'].rolling(window=26).sum()
+    data.loc[:, 'ar'] = (data['h_o_sum'] / data['o_l_sum']) * 100
+    data.loc[:, 'h_c'] = data['high'] - data['close']
+    data.loc[:, 'c_l'] = data['close'] - data['low']
+    data.loc[:, 'h_c_sum'] = data['h_c'].rolling(window=26).sum()
+    data.loc[:, 'c_l_sum'] = data['c_l'].rolling(window=26).sum()
+    data.loc[:, 'br'] = (data['h_c_sum'] / data['c_l_sum']) * 100
+    # EMV
+    data.loc[:, 'm_hl'] = (data['high'].shift(1) + data['low'].shift(1)) / 2
+    data.loc[:, 'emva_em'] = (data['m_price'] - data['m_hl']) * data['h_l'] / data['amount']
+    data.loc[:, 'emv'] = data['emva_em'].rolling(window=14).sum()
+    data.loc[:, 'emva'] = tl.MA(data['emv'], timeperiod=9)
+    # BIAS
+    data.loc[:, 'ma6'] = tl.MA(data['close'], timeperiod=6)
+    data.loc[:, 'ma12'] = tl.MA(data['close'], timeperiod=12)
+    data.loc[:, 'ma24'] = tl.MA(data['close'], timeperiod=24)
+    data.loc[:, 'bias'] = ((data['close'] - data['ma6']) / data['ma6']) * 100
+    data.loc[:, 'bias_12'] = ((data['close'] - data['ma12']) / data['ma12']) * 100
+    data.loc[:, 'bias_24'] = ((data['close'] - data['ma24']) / data['ma24']) * 100
+    # VOL
+    data.loc[:, 'vol_5'] = tl.MA(data['volume'], timeperiod=5)
+    data.loc[:, 'vol_10'] = tl.MA(data['volume'], timeperiod=10)
+    # MA
+    data.loc[:, 'ma20'] = tl.MA(data['close'], timeperiod=20)
+    data.loc[:, 'ma200'] = tl.MA(data['close'], timeperiod=200)
 
-    data_stat.loc[:, 'ext_0'] = data_stat['close'] - data_stat['close'].shift(1)
-    data_stat.loc[:, 'ext_1'] = 0
-    data_stat.loc[data_stat['ext_0'] > 0, 'ext_1'] = 1
-    data_stat.loc[:, 'ext_2'] = data_stat['ext_1'].rolling(window=12).sum()
-    data_stat.loc[:, 'psy'] = (data_stat['ext_2'] / 12.0) * 100
+    if threshold is not None:
+        data = data.tail(n=threshold).copy()
 
-    data_stat.loc[:, 'h_o'] = data_stat['high'] - data_stat['open']
-    data_stat.loc[:, 'o_l'] = data_stat['open'] - data_stat['low']
-    data_stat.loc[:, 'h_o_sum'] = data_stat['h_o'].rolling(window=26).sum()
-    data_stat.loc[:, 'o_l_sum'] = data_stat['o_l'].rolling(window=26).sum()
-    data_stat.loc[:, 'ar'] = (data_stat['h_o_sum'] / data_stat['o_l_sum']) * 100
-    data_stat.loc[:, 'h_c'] = data_stat['high'] - data_stat['close']
-    data_stat.loc[:, 'c_l'] = data_stat['close'] - data_stat['low']
-    data_stat.loc[:, 'h_c_sum'] = data_stat['h_c'].rolling(window=26).sum()
-    data_stat.loc[:, 'c_l_sum'] = data_stat['c_l'].rolling(window=26).sum()
-    data_stat.loc[:, 'br'] = (data_stat['h_c_sum'] / data_stat['c_l_sum']) * 100
-
-    data_stat.loc[:, 'emva_a'] = (data_stat['high'] + data_stat['low']) / 2
-    data_stat.loc[:, 'emva_b'] = (data_stat['high'].shift(1) + data_stat['low'].shift(1)) / 2
-    data_stat.loc[:, 'emva_c'] = data_stat['high'] - data_stat['low']
-    data_stat.loc[:, 'emva_em'] = (data_stat['emva_a'] - data_stat['emva_b']) * data_stat['emva_c'] / data_stat[
-        'amount']
-    data_stat.loc[:, 'emv'] = data_stat['emva_em'].rolling(window=14).sum()
-    data_stat.loc[:, 'emva'] = tl.MA(data_stat['emv'], timeperiod=9)
-
-    data_stat.loc[:, 'ma6'] = tl.MA(data_stat['close'], timeperiod=6)
-    data_stat.loc[:, 'ma12'] = tl.MA(data_stat['close'], timeperiod=12)
-    data_stat.loc[:, 'ma24'] = tl.MA(data_stat['close'], timeperiod=24)
-    data_stat.loc[:, 'bias'] = ((data_stat['close'] - data_stat['ma6']) / data_stat['ma6']) * 100
-    data_stat.loc[:, 'bias_12'] = ((data_stat['close'] - data_stat['ma12']) / data_stat['ma12']) * 100
-    data_stat.loc[:, 'bias_24'] = ((data_stat['close'] - data_stat['ma24']) / data_stat['ma24']) * 100
-
-    data_stat.loc[:, 'roc'] = tl.ROC(data_stat['close'], timeperiod=12)
-    data_stat.loc[:, 'rocma'] = tl.MA(data_stat['roc'], timeperiod=6)
-    data_stat.loc[:, 'rocema'] = tl.EMA(data_stat['rocma'], timeperiod=9)
-
-    return data_stat
-
-
-def get_indicators_talib(data, end_date=None, threshold=60):
-    if end_date is not None:
-        mask = (data['date'] <= end_date)
-        data = data.loc[mask]
-    df = data.tail(n=threshold).copy()
-    df.loc[:, "volume"] = data["volume"] * 100  # 成交量单位从手变成股。
-
-    df.loc[:, 'rsi'] = tl.RSI(df['close'], timeperiod=14)
-    df.loc[:, 'rsi_6'] = tl.RSI(df['close'], timeperiod=6)
-    df.loc[:, 'rsi_12'] = tl.RSI(df['close'], timeperiod=12)
-    df.loc[:, 'rsi_24'] = tl.RSI(df['close'], timeperiod=24)
-
-    df.loc[:, 'macd'], df.loc[:, 'macds'], df.loc[:, 'macdh'] = tl.MACD(df['close'], fastperiod=12, slowperiod=26,
-                                                                      signalperiod=9)
-
-    df.loc[:, 'kdjk'], df.loc[:, 'kdjd'] = tl.STOCH(df['high'], df['low'], df['close'], fastk_period=5,
-                                                    slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-    df.loc[:, 'kdjj'] = 3 * df['kdjk'] - 2 * df['kdjd']
-
-    df.loc[:, 'm_price'] = (df['high'] + df['low']) / 2
-    df.loc[:, 'h_m'] = df['high'] - df['m_price'].shift(1)
-    df.loc[:, 'm_l'] = df['m_price'].shift(1) - df['low']
-    df.loc[:, 'h_m_sum'] = df['h_m'].rolling(window=26).sum()
-    df.loc[:, 'm_l_sum'] = df['m_l'].rolling(window=26).sum()
-    df.loc[:, 'cr'] = (df['h_m_sum'] / df['m_l_sum']) * 100
-    df.loc[:, 'cr-ma1'] = tl.MA(df['cr'], timeperiod=5)
-    df.loc[:, 'cr-ma2'] = tl.MA(df['cr'], timeperiod=10)
-    df.loc[:, 'cr-ma3'] = tl.MA(df['cr'], timeperiod=20)
-    df.loc[:, 'boll_ub'], df.loc[:, 'boll'], df.loc[:, 'boll_lb'] = tl.BBANDS(df['close'], timeperiod=20, nbdevup=2,
-                                                                              nbdevdn=2, matype=0)
-    df.loc[:, 'trix'] = tl.TRIX(df['close'], timeperiod=12)
-    df.loc[:, 'trix_20_sma'] = tl.MA(df['trix'], timeperiod=20)
-
-    df.loc[:, 'ma10'] = tl.MA(df['close'], timeperiod=10)
-    df.loc[:, 'ma50'] = tl.MA(df['close'], timeperiod=50)
-    df.loc[:, 'dma'] = df['ma10'] - df['ma50']
-    df.loc[:, 'dma_10_sma'] = tl.MA(df['dma'], timeperiod=10)
-
-    df.loc[:, 'cci'] = tl.CCI(df['high'], df['low'], df['close'], timeperiod=14)
-    df.loc[:, 'cci_84'] = tl.CCI(df['high'], df['low'], df['close'], timeperiod=84)
-
-    df.loc[:, 'h_6'] = df['high'].rolling(window=6).max()
-    df.loc[:, 'l_6'] = df['low'].rolling(window=6).min()
-    df.loc[:, 'wr_6'] = ((df['h_6'] - df['close']) / (df['h_6'] - df['l_6'])) * 100
-    df.loc[:, 'h_10'] = df['high'].rolling(window=10).max()
-    df.loc[:, 'l_10'] = df['low'].rolling(window=10).min()
-    df.loc[:, 'wr_10'] = ((df['h_10'] - df['close']) / (df['h_10'] - df['l_10'])) * 100
-    df.loc[:, 'h_14'] = df['high'].rolling(window=14).max()
-    df.loc[:, 'l_14'] = df['low'].rolling(window=14).min()
-    df.loc[:, 'wr_14'] = ((df['h_14'] - df['close']) / (df['h_14'] - df['l_14'])) * 100
-
-    df.loc[:, 'pdi'] = tl.PLUS_DI(df['high'], df['low'], df['close'], timeperiod=14)
-    df.loc[:, 'mdi'] = tl.MINUS_DI(df['high'], df['low'], df['close'], timeperiod=14)
-    df.loc[:, 'dx'] = tl.DX(df['high'], df['low'], df['close'], timeperiod=14)
-    df.loc[:, 'adx'] = tl.ADX(df['high'], df['low'], df['close'], timeperiod=6)
-    df.loc[:, 'adxr'] = tl.ADXR(df['high'], df['low'], df['close'], timeperiod=6)
-
-    df.loc[:, 'vol_5'] = tl.MA(df['volume'], timeperiod=5)
-    df.loc[:, 'vol_10'] = tl.MA(df['volume'], timeperiod=10)
-
-    df.loc[:, 'obv'] = tl.OBV(df['close'], df['volume'])
-    df.loc[:, 'sar'] = tl.SAR(df['high'], df['low'])
-
-    df.loc[:, 'prev_close'] = df['close'].shift(1)
-    df.loc[:, 'ext_0'] = df['close'] - df['prev_close']
-    df.loc[:, 'ext_1'] = 0
-    df.loc[df['ext_0'] > 0, 'ext_1'] = 1
-    df.loc[:, 'ext_2'] = df['ext_1'].rolling(window=12).sum()
-    df.loc[:, 'psy'] = (df['ext_2'] / 12.0) * 100
-
-    df.loc[:, 'h_o'] = df['high'] - df['open']
-    df.loc[:, 'o_l'] = df['open'] - df['low']
-    df.loc[:, 'h_o_sum'] = df['h_o'].rolling(window=26).sum()
-    df.loc[:, 'o_l_sum'] = df['o_l'].rolling(window=26).sum()
-    df.loc[:, 'ar'] = (df['h_o_sum'] / df['o_l_sum']) * 100
-    df.loc[:, 'h_c'] = df['high'] - df['close']
-    df.loc[:, 'c_l'] = df['close'] - df['low']
-    df.loc[:, 'h_c_sum'] = df['h_c'].rolling(window=26).sum()
-    df.loc[:, 'c_l_sum'] = df['c_l'].rolling(window=26).sum()
-    df.loc[:, 'br'] = (df['h_c_sum'] / df['c_l_sum']) * 100
-
-    df.loc[:, 'm_hl'] = (df['high'].shift(1) + df['low'].shift(1)) / 2
-    df.loc[:, 'h_l'] = df['high'] - df['low']
-    df.loc[:, 'emva_em'] = (df['m_price'] - df['m_hl']) * df['h_l'] / df['amount']
-    df.loc[:, 'emv'] = df['emva_em'].rolling(window=14).sum()
-    df.loc[:, 'emva'] = tl.MA(df['emv'], timeperiod=9)
-
-    df.loc[:, 'ma6'] = tl.MA(df['close'], timeperiod=6)
-    df.loc[:, 'ma12'] = tl.MA(df['close'], timeperiod=12)
-    df.loc[:, 'ma24'] = tl.MA(df['close'], timeperiod=24)
-    df.loc[:, 'bias'] = ((df['close'] - df['ma6']) / df['ma6']) * 100
-    df.loc[:, 'bias_12'] = ((df['close'] - df['ma12']) / df['ma12']) * 100
-    df.loc[:, 'bias_24'] = ((df['close'] - df['ma24']) / df['ma24']) * 100
-
-    df.loc[:, 'roc'] = tl.ROC(df['close'], timeperiod=12)
-    df.loc[:, 'rocma'] = tl.MA(df['roc'], timeperiod=6)
-    df.loc[:, 'rocema'] = tl.EMA(df['rocma'], timeperiod=9)
-
-    df.loc[:, 'av'] = df['volume'].where(df['p_change'] > 0, 0, inplace=False)
-    df.loc[:, 'avs'] = df['av'].rolling(min_periods=1, window=26, center=False).sum()
-    df.loc[:, 'bv'] = df['volume'].where(df['p_change'] < 0, 0, inplace=False)
-    df.loc[:, 'bvs'] = df['bv'].rolling(min_periods=1, window=26, center=False).sum()
-    df.loc[:, 'cv'] = df['volume'].where(df['p_change'] == 0, 0, inplace=False)
-    df.loc[:, 'cvs'] = df['cv'].rolling(min_periods=1, window=26, center=False).sum()
-    df.loc[:, 'vr'] = (df['avs'] + df['cvs'] / 2) / (df['bvs'] + df['cvs'] / 2) * 100
-    df.loc[:, 'vr_6_sma'] = tl.MA(df['vr'], timeperiod=6)
-
-    df.loc[:, 'h_pc'] = (df['high'] - df['prev_close']).abs()
-    df.loc[:, 'l_pc'] = (df['prev_close'] - df['low']).abs()
-    df.loc[:, 'tr'] = df.loc[:, ['h_l', 'h_pc', 'l_pc']].T.max()
-    df.loc[:, 'atr'] = df['tr'].ewm(ignore_na=False, alpha=1.0 / 14, min_periods=0, adjust=True).mean()
-
-    return df
+    return data
 
 
-def get_indicator_tail(code_name, data, stock_column, date=None, threshold=60):
+def get_indicator(code_name, data, stock_column, date=None, calc_threshold=250):
     try:
         if date is None:
             end_date = code_name[0]
@@ -195,7 +164,7 @@ def get_indicator_tail(code_name, data, stock_column, date=None, threshold=60):
                 stock_data_list.append(0)
             return pd.Series(stock_data_list, index=stock_column)
 
-        stockStat = get_indicators(data, end_date=end_date, threshold=threshold)
+        stockStat = get_indicators(data, end_date=end_date, threshold=1, calc_threshold=calc_threshold)
         # 增加空判断，如果是空返回 0 数据。
         if stockStat is None:
             for i in range(len(stock_column) - 2):
