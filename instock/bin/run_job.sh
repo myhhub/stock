e55fdfdb809d7063baecaa7aad3246aa0da1f00f
myhhub <yh_ma@qq.com>
2023-04-21 12:06:00 +0800

    Dockerfile优化

--- instock/bin/run_job.sh
+++ instock/bin/run_job.sh
@@ -3,10 +3,10 @@
 
 # https://stackoverflow.com/questions/27771781/how-can-i-access-docker-set-environment-variables-from-a-cron-job
 # 解决环境变量输出问题。
-printenv | grep -v "no_proxy" >> /etc/environment
+#printenv | grep -v "no_proxy" >> /etc/environment
 
 # 第一次后台执行日数据。
-nohup bash /data/InStock/cron/cron.daily/run_daily &
+nohup bash /etc/cron.hourly/run_daily &
 
 #启动cron服务。在前台
 /usr/sbin/cron -f
