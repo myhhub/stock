014f8a355f6721901e605884940c21f53499aabf
myhhub <yh_ma@qq.com>
2023-04-21 10:49:23 +0800

    Dockerfile优化

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 rm -rf InStock
-rsync -av --progress ../../InStock . --exclude .git --exclude .idea --exclude __pycache__ --exclude .gitignore --exclude InStock/img --exclude InStock/docker --exclude instock/cache --exclude instock/log --exclude instock/test
+rsync -av --progress ../../InStock . --exclude .git --exclude .idea --exclude __pycache__ --exclude .gitignore --exclude InStock/cron --exclude InStock/img --exclude InStock/docker --exclude instock/cache --exclude instock/log --exclude instock/test
+rm -rf cron
+cp -r ../../InStock/cron .
 
 DOCKER_NAME=mayanghua/instock
 TAG1=$(date "+%Y%m")
