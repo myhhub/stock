e6d1adde14e587cc2fb961568a2c6decf37d26f5
myhhub <yh_ma@qq.com>
2023-04-21 12:59:47 +0800

    Dockerfile优化

--- instock/bin/run_web.sh
+++ instock/bin/run_web.sh
@@ -2,8 +2,8 @@
 
 /usr/local/bin/python3 /data/InStock/instock/web/web_service.py
 
-echo ------Web服务已启动，请不要关闭------
-echo 访问地址 : http://localhost:9988/
+#echo ------Web服务已启动，请不要关闭------
+#echo 访问地址 : http://localhost:9988/
 
 #启动cron服务。在前台
 /usr/sbin/cron -f
