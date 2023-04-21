e71e06e830746bbe8a58f2fb09d6adaf4eceeccc
myhhub <yh_ma@qq.com>
2023-04-21 12:54:46 +0800

    Dockerfile优化

--- instock/bin/run_job.sh
+++ instock/bin/run_job.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+#export PYTHONPATH=/data/InStock
 
-# https://stackoverflow.com/questions/27771781/how-can-i-access-docker-set-environment-variables-from-a-cron-job
-# 解决环境变量输出问题。
-#printenv | grep -v "no_proxy" >> /etc/environment
+echo ------整体作业，支持批量作业------
+echo 当前时间作业 python execute_daily_job.py
+echo 1个时间作业 python execute_daily_job.py 2023-03-01
+echo N个时间作业 python execute_daily_job.py 2023-03-01,2023-03-02
+echo 区间作业 python execute_daily_job.py 2023-03-01 2023-03-21
+echo ------单功能作业，除了创建数据库，其他都支持批量作业------
+echo 创建数据库作业 python init_job.py
+echo 基础数据作业 python basic_data_daily_job.py
+echo 指标数据作业 python indicators_data_daily_job.py
+echo K线形态作业 python klinepattern_data_daily_job.py
+echo 策略数据作业 python strategy_data_daily_job.py
+echo 回测数据 python backtest_data_daily_job.py
+echo ------正在执行作业中，请等待------
 
-# 第一次后台执行日数据。
-nohup bash /etc/cron.daily/run_daily &
-
-#启动cron服务。在前台
-/usr/sbin/cron -f
+/usr/local/bin/python3 /data/InStock/instock/web/execute_daily_job.py
--- instock/bin/run_web.sh
+++ instock/bin/run_web.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#export PYTHONIOENCODING=utf-8
-#export LANG=zh_CN.UTF-8
-#export PYTHONPATH=/data/InStock
-#export LC_CTYPE=zh_CN.UTF-8
-
 /usr/local/bin/python3 /data/InStock/instock/web/web_service.py
+
+echo ------Web服务已启动，请不要关闭------
+echo 访问地址 : http://localhost:9988/
+
+#启动cron服务。在前台
+/usr/sbin/cron -f
