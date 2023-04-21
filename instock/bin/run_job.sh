461caa0ad7f53241932278382269f1a7ae26e040
myhhub <yh_ma@qq.com>
2023-04-21 13:48:05 +0800

    修改说明

--- instock/bin/run_job.sh
+++ instock/bin/run_job.sh
@@ -2,18 +2,18 @@
 
 #export PYTHONPATH=/data/InStock
 
-/usr/local/bin/python3 /data/InStock/instock/web/execute_daily_job.py
+/usr/local/bin/python3 /data/InStock/instock/job/execute_daily_job.py
 
-#------整体作业，支持批量作业------
-#当前时间作业 python execute_daily_job.py
-#1个时间作业 python execute_daily_job.py 2023-03-01
-#N个时间作业 python execute_daily_job.py 2023-03-01,2023-03-02
-#区间作业 python execute_daily_job.py 2023-03-01 2023-03-21
-#------单功能作业，除了创建数据库，其他都支持批量作业------
-#创建数据库作业 python init_job.py
-#基础数据作业 python basic_data_daily_job.py
-#指标数据作业 python indicators_data_daily_job.py
-#K线形态作业 python klinepattern_data_daily_job.py
-#策略数据作业 python strategy_data_daily_job.py
-#回测数据 python backtest_data_daily_job.py
-#------正在执行作业中，请等待------
+echo ------整体作业，支持批量作业------
+echo 当前时间作业 python execute_daily_job.py
+echo 1个时间作业 python execute_daily_job.py 2023-03-01
+echo N个时间作业 python execute_daily_job.py 2023-03-01,2023-03-02
+echo 区间作业 python execute_daily_job.py 2023-03-01 2023-03-21
+echo ------单功能作业，除了创建数据库，其他都支持批量作业------
+echo 创建数据库作业 python init_job.py
+echo 基础数据作业 python basic_data_daily_job.py
+echo 指标数据作业 python indicators_data_daily_job.py
+echo K线形态作业 python klinepattern_data_daily_job.py
+echo 策略数据作业 python strategy_data_daily_job.py
+echo 回测数据 python backtest_data_daily_job.py
+echo ------正在执行作业中，请等待------
--- instock/bin/run_web.sh
+++ instock/bin/run_web.sh
@@ -4,6 +4,6 @@
 #启动cron服务。在前台
 /usr/sbin/cron -f
 
-#------Web服务已启动，请不要关闭------
-#访问地址 : http://localhost:9988/
+echo ------Web服务已启动，请不要关闭------
+echo 访问地址 : http://localhost:9988/
 
