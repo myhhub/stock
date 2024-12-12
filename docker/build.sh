82df24de5e7a4a3a0ec5d12c2ded8aeba06d75a4
beer <beer5214@126.com>
2024-12-12 16:18:20 +0800

    imrpvoe Dockerfile. support arm64

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 rm -rf stock
 rsync -av --progress ../../stock . --exclude .git --exclude .idea --exclude *.md --exclude *.bat --exclude __pycache__ --exclude .gitignore --exclude stock/cron --exclude stock/img --exclude stock/docker --exclude instock/cache --exclude instock/log --exclude instock/test
 rm -rf cron
@@ -11,8 +13,3 @@ TAG2=latest
 
 echo " docker build -f Dockerfile -t ${DOCKER_NAME} ."
 docker build -f Dockerfile -t ${DOCKER_NAME}:${TAG1} -t ${DOCKER_NAME}:${TAG2} .
-echo "#################################################################"
-echo " docker push ${DOCKER_NAME} "
-
-docker push ${DOCKER_NAME}:${TAG1}
-docker push ${DOCKER_NAME}:${TAG2}
